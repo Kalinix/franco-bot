26a9c1aefdc7615c3ba6159a9adec993bd95dd40
Kalinix <95724069+Kalinix@users.noreply.github.com>
2022-02-14 17:40:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ async def on_message(message):
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'Pong {round(client.latency * 1000)}')
+    await ctx.send(f'Pong {round(client.latency * 1000)}ms')
 
 @client.command()
 async def limpia(ctx, amount):
