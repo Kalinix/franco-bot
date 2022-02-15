31a2f61696ad9d51b5545ee0f34d6e106551b6b7
Kalinix <95724069+Kalinix@users.noreply.github.com>
2022-02-15 18:09:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -84,6 +84,6 @@ async def com(ctx, member:discord.Member, *, mensaje):
 @commands.has_guild_permissions(administrator = True)
 async def inv(ctx):
     inv = await ctx.channel.create_invite(max_age = 300)
-    ctx.send('Aquí tienes una invitación ' + str (inv))
+    await ctx.send('Aquí tienes una invitación ' + inv)
 
 client.run(TOKEN)
